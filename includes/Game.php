887b5759eff47f4e0a2dbad60299ead87f2cd7c1
Ching Voon <chingvoon@etctech.com.my>
2023-11-06 01:42:53 +0800

    Change alphaber to number, change sequence of type and number

--- includes/Game.php
+++ includes/Game.php
@@ -19,14 +19,14 @@
             // initial declare the cards
             $init_cards = array();
             // Card 2 to 9 are, as it is, 1=A,10=X,11=J,12=Q,13=K
-            $number_of_cards = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'X', 'J', 'Q', 'K'];
+            $number_of_cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'X', 'J', 'Q', 'K'];
             // Spade = S, Heart = H, Diamond = D, Club = C
             $types_of_cards = ['S', 'H', 'C', 'D'];
 
             foreach($number_of_cards as $number) {
                 foreach($types_of_cards as $type) {
                     // push all available cards into array
-                    array_push($init_cards, $number.'-'.$type);
+                    array_push($init_cards, $type.'-'.$number);
                 }
             }
             // return all available cards
